a7e08b138bd5c8eeb75cf895ede53b6f118e0f83
Chris <christech1117@gmail.com>
2018-05-19 02:12:38 +0800

    病人列表

--- src/views/person/User.vue
+++ src/views/person/User.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="app-container">
+    <div class="filter-container">
+      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">搜索</el-button>
+      <el-button class="filter-item" style="margin-left: 10px;" @click="handleCreate" type="primary" icon="el-icon-edit">新增</el-button>
+      <el-button class="filter-item" type="primary" :loading="downloadLoading" v-waves icon="el-icon-download" @click="handleDownload">下載</el-button>
+    </div>
     <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>
       <el-table-column align="center" label='代號' width="105">
         <template slot-scope="scope">
@@ -13,25 +18,37 @@
       </el-table-column>
       <el-table-column label="姓名" align="center">
         <template slot-scope="scope">
-          {{scope.row.name}}
+          <span class="link-type" @click="handleUpdate(scope.row)">{{scope.row.name}}</span>
         </template>
       </el-table-column>
       <el-table-column label="區館" align="center">
         <template slot-scope="scope">
-          {{scope.row.title}}
+          {{scope.row.area_manage}}
         </template>
       </el-table-column>
       <el-table-column class-name="status-col" label="狀態" align="center">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.type | statusFilter">{{scope.row.type}}</el-tag>
+          <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="工具" align="center">
+      <el-table-column class-name="status-col" label="操作" align="center">
         <template slot-scope="scope">
-          {{scope.row.name}}
+          <el-button type="success" size="mini" @click="handleUpdate(scope.row)">選擇</el-button>
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">編輯</el-button>
+          <el-button type="danger" size="mini" @click="handleUpdate(scope.row)">刪除</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog title="Reading statistics" :visible.sync="dialogFormVisible">
+      <el-table :data="list" border fit highlight-current-row style="width: 100%">
+        <el-table-column prop="key" label="Channel"> </el-table-column>
+        <el-table-column prop="pv" label="Pv"> </el-table-column>
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogFormVisible = false">儲存</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -42,15 +59,19 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
+      listLoading: true,
+      textMap: {
+        update: 'Edit',
+        create: 'Create'
+      },
+      dialogFormVisible: false,
     }
   },
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
+        服務中: 'success',
+        暫停: 'danger'
       }
       return statusMap[status]
     }
@@ -63,9 +84,19 @@ export default {
       this.listLoading = true
       getList(this.listQuery).then(response => {
         this.list = response.data.items
+        this.total = response.data.total
         this.listLoading = false
       })
-    }
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.temp.timestamp = new Date(this.temp.timestamp)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
   }
 }
 </script>
