1426fc13ff9b3897a1f1341ad7d69a181acb7ec6
Chris <christech1117@gmail.com>
2018-06-01 08:46:17 +0800

    add passport component

---
+++ src/views/passport/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <clients></clients>
+    <!-- <authorized-clients></authorized-clients>
+    <personal-access-tokens></personal-access-tokens> -->
+  </div>
+</template>
+<script>
+import Clients from '@/views/passport/Clients'
+import AuthorizedClients from '@/views/passport/AuthorizedClients'
+import PersonalAccessTokens from '@/views/passport/PersonalAccessTokens'
+
+export default {
+  components: { Clients, AuthorizedClients, PersonalAccessTokens },
+  data() {
+    return {
+    }
+  },
+  created() {
+    console.log(Clients)
+  }
+}
+</script>
